9199e852d36117c1be6d8cd5d61794b8cee10552
Aishwary-Pratap-Singh <aishwarypsingh@gmail.com>
2022-04-27 10:31:56 +0530

    added resillience4j for fault tolerance

--- productservice/src/main/java/com/example/productservice/controller/ProductRestController.java
+++ productservice/src/main/java/com/example/productservice/controller/ProductRestController.java
@@ -4,6 +4,7 @@ import com.example.productservice.model.Coupon;
 import com.example.productservice.model.Product;
 import com.example.productservice.repo.ProductRepo;
 import com.example.productservice.restclients.CouponClient;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +21,16 @@ public class ProductRestController {
     private ProductRepo repo;
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
+    @Retry(name="product-api", fallbackMethod = "handleError")
     public Product create(@RequestBody Product product){
         Coupon coupon = couponClient.getCoupon(product.getCouponCode());
         product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
         return repo.save(product);
+    }
+
+    public Product handleError(Product product, Exception exception){
+        System.out.println("Inside handle Error");
+        return product;
     }
 
 }
